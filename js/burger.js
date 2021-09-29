7128f497351486185968e74dfc024b808775c41b
GAAdel <32236445+GAAdel@users.noreply.github.com>
2021-09-29 16:46:56 +0300

    closing burger

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector('.humburger-menu');
 const menuElem = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu-list__link');
 
 const toggleMenu = () => {
   menuElem.classList.toggle('menu-active');
@@ -8,4 +9,11 @@ const toggleMenu = () => {
 
 burger.addEventListener('click', () => {
   toggleMenu(); 
+})
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menuElem.classList.remove('menu-active');
+    burger.classList.remove('humburger-menu-active');
+  })
 })
